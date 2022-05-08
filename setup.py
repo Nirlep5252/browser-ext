1f8f13ec1363dbe15b959186ce6d771bd6422a5d
nirlep5252 <hellonirlep@gmail.com>
2022-05-08 20:58:31 +0530

    first commit with pepega setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='browser-ext',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.example.com',
+    author='me',
+    author_email='me@example.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
